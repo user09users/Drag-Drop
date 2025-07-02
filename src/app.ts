1b76994fad28f93cc4f677f85b02dc6a9aa14d33
Mkh <example@gmail.com>
2025-07-02 11:38:31 +0200

    ES Modules

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,6 @@
-/// <reference path= "components/project-list.ts"/>
-/// <reference path= "components/project-input.ts"/>
+import { ProjectInput } from "./components/project-input";
+import { ProjectList } from "./components/project-list";
 
-namespace App {
-  // ProjectInput Class
-
-  new ProjectInput();
-  new ProjectList("active");
-  new ProjectList("finished");
-}
+new ProjectInput();
+new ProjectList("active");
+new ProjectList("finished");
